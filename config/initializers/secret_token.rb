26ebd1407fa1c20a4dbe1297c9ea09a8618385de
suisho <suisho@suisho-no-MacBook-Air.local>
2014-01-24 13:09:50 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nanoblockdb::Application.config.secret_key_base = '9720ada83c8616e4b9226bbf7889043670ebd5381dc4e072556fcf9ce20d42610cfc74d78f6853580232846995bcebc3e5d05c76a7c0f4740b67f34b6fca4692'
