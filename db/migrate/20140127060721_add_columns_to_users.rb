a2f505e53e0d26ff8fe123076ba0093512ec58c1
suisho <suisho000000@gmail.com>
2014-01-27 16:10:29 +0900

    reset db

--- db/migrate/20140127060721_add_columns_to_users.rb
+++ db/migrate/20140127060721_add_columns_to_users.rb
@@ -1,5 +1,4 @@
 class AddColumnsToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :provider, :string
   end
 end
---
+++ db/migrate/20140127160000_add_omni_to_user.rb
@@ -0,0 +1,9 @@
+class AddOmniToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :uid, :integer
+    add_column :users, :provider, :string
+    add_column :users, :name, :string
+    add_column :users, :image, :string
+
+  end
+end
