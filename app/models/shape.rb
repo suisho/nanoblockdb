81e9280b18c832a02c67f0fc74bd6cf74a33cae2
suisho <suisho000000@gmail.com>
2014-01-27 21:45:57 +0900

    add default values

--- app/models/shape.rb
+++ app/models/shape.rb
@@ -1,2 +1,8 @@
 class Shape < ActiveRecord::Base
+  def short_side_dot_num
+   read_attribute(:short_side_dot_num) || read_attribute(:short_side_size)
+  end
+  def long_side_dot_num
+   read_attribute(:long_side_dot_num) || read_attribute(:long_side_size)
+  end
 end
