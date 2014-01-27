d951ba1937f76ae3cdb2b61c930a627465c37569
suisho <suisho000000@gmail.com>
2014-01-27 18:56:47 +0900

    fix typo

--- app/models/color.rb
+++ app/models/color.rb
@@ -1,4 +1,4 @@
 class Color < ActiveRecord::Base
-  validates :name, :precense => ture, format: { with: /[a-z _0-9]/ }
-  validates :code, :precense => ture, format: { with: /[0-9a-f]+/i }
+  validates :name, :presence => true, format: { with: /[a-z _0-9]/ }
+  validates :code, :presence => true, format: { with: /[0-9a-f]+/i }
 end
