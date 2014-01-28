8e95999a785f63f99e21f55305a7c63769705d80
suisho <suisho000000@gmail.com>
2014-01-28 18:32:23 +0900

    add i18n detect function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  before_filter :set_locale
+  
+  def set_locale
+    I18n.locale = request.env['HTTP_ACCEPT_LANGUAGE'].split(",").first
+  end
+  
 end
